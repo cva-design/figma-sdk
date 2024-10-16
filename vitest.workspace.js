30710565130f85dfcec183d7ef7e1b8fce179888
Saulo Vallory <me@saulo.engineer>
2024-10-16 11:59:16 -0300

    fix: biome and vitest configs
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- vitest.workspace.js
+++ vitest.workspace.js
@@ -1,7 +1,5 @@
 import { defineWorkspace } from 'vitest/config'
 
 export default defineWorkspace([
-  "./vite.config.ts",
-  "./dist/config/vite.config.js",
-  "./src/lib/config/vite.config.ts"
+  "./vite.config.ts"
 ])
