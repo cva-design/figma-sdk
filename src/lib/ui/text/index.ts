192c0eccefbf60bda148f4df25e7e8b8869d5684
Saulo Vallory <me@saulo.engineer>
2024-10-25 20:58:39 -0300

    feat: add label, text, paragraph and link components
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

---
+++ src/lib/ui/text/index.ts
@@ -0,0 +1,4 @@
+export { default as Label } from './label.svelte';
+export { default as Link } from './link.svelte';
+export { default as Paragraph } from './paragraph.svelte';
+export { default as Text } from './text.svelte';
---
+++ src/lib/ui/text/label.svelte
@@ -0,0 +1,51 @@
+<script context="module" lang="ts">
+	import './text.scss';
+	export type Size = 'small' | 'medium' | 'large';
+	export type Weight = 'default' | 'strong';
+	export type Align = 'start' | 'center' | 'end';
+</script>
+
+<script lang="ts">
+	import { cva, type VariantProps } from 'class-variance-authority';
+	import type { HTMLLabelAttributes } from 'svelte/elements';
+
+	const label = cva('fp-Text', {
+		variants: {
+			size: {
+				small: 'fp-size-small',
+				medium: 'fp-size-medium',
+				large: 'fp-size-large'
+			},
+			weight: {
+				default: 'fp-weight-default',
+				strong: 'fp-weight-strong'
+			},
+			align: {
+				start: 'fp-align-start',
+				center: 'fp-align-center',
+				end: 'fp-align-end'
+			},
+			block: {
+				true: 'fp-block'
+			}
+		},
+		defaultVariants: {
+			size: 'medium',
+			weight: 'default',
+			align: 'start'
+		}
+	});
+
+	interface $$Props extends HTMLLabelAttributes, VariantProps<typeof label> {
+		block?: boolean;
+	}
+
+	export let size: $$Props['size'] = undefined;
+	export let weight: $$Props['weight'] = undefined;
+	export let align: $$Props['align'] = undefined;
+	export let block: boolean = false;
+</script>
+
+<label class={label({ size, weight, align, block, class: $$props.class })} {...$$restProps}>
+	<slot />
+</label>
