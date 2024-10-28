a30e996cc667b53b46b8afe9df52a512c41ef635
Saulo Vallory <me@saulo.engineer>
2024-10-28 11:50:47 -0300

    WIP: tree and layer-tree components
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

---
+++ src/lib/ui/layer-tree/Action.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import type { HTMLAttributes } from 'svelte/elements';
+
+	interface $$Props extends HTMLAttributes<HTMLButtonElement> {
+		action: {
+			id: string;
+			icon: string;
+			tooltip: string;
+			active?: boolean;
+			enabled?: boolean;
+		};
+	}
+
+	export let action: $$Props['action'];
+</script>
+
+<button
+	class="action"
+	class:active={action.active}
+	disabled={!action.enabled}
+	title={action.tooltip}
+	{...$$restProps}
+>
+	{@html action.icon}
+</button>
+
+<style>
+	.action {
+		background: none;
+		border: none;
+		cursor: pointer;
+		padding: 2px;
+		margin: 0 2px;
+		color: var(--figma-color-icon-secondary);
+	}
+
+	.action:disabled {
+		opacity: 0.5;
+		cursor: not-allowed;
+	}
+
+	.action.active {
+		color: var(--figma-color-icon);
+	}
+
+	.action:hover:not(:disabled) {
+		color: var(--figma-color-icon);
+	}
+</style>
