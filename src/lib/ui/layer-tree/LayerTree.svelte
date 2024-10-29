7e065cdd1529544948b19027b88fafb29eef0aff
Alberto Nunes <bisneto.alberto@gmail.com>
2024-10-29 11:29:23 -0300

    feat: add tree property

--- src/lib/ui/layer-tree/LayerTree.svelte
+++ src/lib/ui/layer-tree/LayerTree.svelte
@@ -1,21 +1,31 @@
+<script lang="ts" context="module">
+	import type { Action as ActionType } from '../tree/types';
+	export type LayerTreeData = {
+		children: LayerTreeData[];
+		type: LayerType;
+		name: string;
+		description?: string | undefined;
+		component?: boolean;
+		selected?: boolean;
+		expanded?: boolean;
+		actions?: ActionType[];
+		depth?: number;
+	};
+</script>
+
 <script lang="ts">
 	import { ChevronDownSvg_16, ChevronRightSvg_16 } from '$icons';
 	import type { LayerType } from '../layer/types';
 	import Layer from './Layer.svelte';
 
+	export let tree: LayerTreeData | undefined | null;
 	export let type: LayerType;
 	export let name: string;
 	export let description: string | undefined = undefined;
 	export let component: boolean = false;
 	export let selected: boolean = false;
 	export let expanded: boolean = false;
-	export let actions: Array<{
-		id: string;
-		icon: string;
-		tooltip: string;
-		active?: boolean;
-		enabled?: boolean;
-	}> = [];
+	export let actions: ActionType[] = [];
 	export let depth: number = 0;
 
 	function toggleExpand() {
@@ -23,21 +33,51 @@
 	}
 </script>
 
-<div class="layerTree" style="--depth: {depth}">
-	<div class="layerTree--header">
-		<button class="layerTree--caret" on:click={toggleExpand} class:expanded>
-			{@html expanded ? ChevronDownSvg_16 : ChevronRightSvg_16}
-		</button>
+{#if tree}
+	<div class="layerTree" style="--depth: {depth}">
+		<div class="layerTree--header">
+			{#if tree.children.length > 0}
+				<button class="layerTree--caret" on:click={toggleExpand} class:expanded>
+					{@html expanded ? ChevronDownSvg_16 : ChevronRightSvg_16}
+				</button>
+			{/if}
 
-		<Layer {type} {name} {description} {component} {selected} {actions} on:click />
-	</div>
-
-	{#if expanded}
-		<div class="layerTree--children">
-			<slot />
+			<Layer
+				type={tree.type}
+				name={tree.name}
+				description={tree.description}
+				component={tree.component}
+				selected={tree.selected}
+				actions={tree.actions}
+				on:click
+			/>
 		</div>
-	{/if}
-</div>
+
+		{#if expanded}
+			<div class="layerTree--children">
+				{#each tree.children as child}
+					<svelte:self tree={child} depth={depth + 1} />
+				{/each}
+			</div>
+		{/if}
+	</div>
+{:else}
+	<div class="layerTree" style="--depth: {depth}">
+		<div class="layerTree--header">
+			<button class="layerTree--caret" on:click={toggleExpand} class:expanded>
+				{@html expanded ? ChevronDownSvg_16 : ChevronRightSvg_16}
+			</button>
+
+			<Layer {type} {name} {description} {component} {selected} {actions} on:click />
+		</div>
+
+		{#if expanded}
+			<div class="layerTree--children">
+				<slot />
+			</div>
+		{/if}
+	</div>
+{/if}
 
 <style>
 	.layerTree {
