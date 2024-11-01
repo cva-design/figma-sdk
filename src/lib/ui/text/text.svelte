33ec435ae94d767f1057ce6df1878779c57734d5
Saulo Vallory <me@saulo.engineer>
2024-11-01 04:06:55 -0300

    feat: add inverse option to text component
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- src/lib/ui/text/text.svelte
+++ src/lib/ui/text/text.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { cva, type VariantProps } from 'class-variance-authority';
 	import type { HTMLAttributes } from 'svelte/elements';
-	import './text.scss';
 
 	const text = cva('fp-Text', {
 		variants: {
@@ -21,25 +20,31 @@
 			},
 			block: {
 				true: 'fp-block'
+			},
+			inverse: {
+				true: 'fp-inverse'
 			}
 		},
 		defaultVariants: {
 			size: 'medium',
 			weight: 'default',
-			align: 'start'
+			align: 'start',
+			block: false,
+			inverse: false
 		}
 	});
 
-	interface $$Props extends HTMLAttributes<HTMLSpanElement>, VariantProps<typeof text> {
-		block?: boolean;
-	}
+	interface $$Props extends HTMLAttributes<HTMLSpanElement>, VariantProps<typeof text> {}
 
 	export let size: $$Props['size'] = undefined;
 	export let weight: $$Props['weight'] = undefined;
 	export let align: $$Props['align'] = undefined;
-	export let block: boolean = false;
+	export let block: $$Props['block'] = false;
+	export let inverse: $$Props['inverse'] = false;
 </script>
 
-<span class={`${text({ size, weight, align, block })} ${$$props.class}`}>
+<span class={`${text({ size, weight, align, block, inverse })} ${$$props.class}`}>
 	<slot />
 </span>
+
+<style lang="scss" src="./text.scss"></style>
