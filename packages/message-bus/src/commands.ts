c142e64be4fcdfee4ae6ee96f271f346d5efc523
Saulo Vallory <me@saulo.engineer>
2024-11-27 17:55:25 -0300

    WIP
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- packages/message-bus/src/commands.ts
+++ packages/message-bus/src/commands.ts
@@ -1,20 +1,9 @@
-export interface SomeObject extends Record<string | symbol, unknown> {}
-
-export enum Command {
-	RunTests = "test:command/run-tests",
-}
-
-export interface RunTestsCommand extends SomeObject {
-	pattern: string;
-}
-
-export type AckResult<T = unknown> = T &
-	({ success: true } | { success: false; error: string });
+import type { SomeObject } from '@shared/types';
 
 export interface CommandDefinition<
-	Id extends Command,
-	Message = undefined,
-	Result = void,
+  Id extends string,
+  Message = undefined,
+  Result = void,
 > {
 	$id: Id;
 	$type: "command";
@@ -22,7 +11,8 @@ export interface CommandDefinition<
 	result: Result;
 }
 
-export interface CommandRegistry
-	extends Record<Command, CommandDefinition<Command, SomeObject | undefined>> {
-	[Command.RunTests]: CommandDefinition<Command.RunTests, RunTestsCommand>;
-}
+export type CommandRegistry<TCommand> = {
+  [K in keyof TCommand]: K extends string
+    ? CommandDefinition<K, SomeObject | undefined>
+    : never;
+};
--- packages/ui/src/lib/next/dialog/index.ts
+++ packages/ui/src/lib/next/dialog/index.ts
@@ -2,3 +2,8 @@
 export { default as DialogTitle } from './dialog-title.svelte';
 export { default as Dialog } from './dialog.svelte';
 //@endindex
+
+//@index(['./[^\.]+.svelte'], (f, {pascalCase}) => `export * from '${f.path}${f.ext}';`)
+export * from './dialog-title.svelte';
+export * from './dialog.svelte';
+//@endindex
