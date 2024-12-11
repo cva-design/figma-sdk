58d592ef362c405b3a0d66124687f69d1b7cdabf
Thiago Vallory <soeutenho@gmail.com>
2024-12-11 07:23:18 -0300

    add: icons ui3

--- packages/ui/src/lib/icons/ui3/index.ts
+++ packages/ui/src/lib/icons/ui3/index.ts
@@ -29,6 +29,16 @@
  */
 
 //@index('./*.{png,jpg,jpeg,svg,gif}', (f, _) => `export { default as ${_.pascalCase(f.name).replace(/(_[0-9]+|$)/, 'Svg$1')} } from '${f.path}${f.ext}?raw';`)
+export { default as BooleanPropertySvg_16 } from './boolean-property-16.svg?raw';
+export { default as BooleanPropertySvg_24 } from './boolean-property-24.svg?raw';
+export { default as ColorPalettePropertySvg_16 } from './color-palette-property-16.svg?raw';
+export { default as ColorPalettePropertySvg_24 } from './color-palette-property-24.svg?raw';
+export { default as ColorPropertySvg_16 } from './color-property-16.svg?raw';
+export { default as ColorPropertySvg_24 } from './color-property-24.svg?raw';
+export { default as EffectPropertySvg_16 } from './effect-property-16.svg?raw';
+export { default as EffectPropertySvg_24 } from './effect-property-24.svg?raw';
+export { default as GridPropertySvg_16 } from './grid-property-16.svg?raw';
+export { default as GridPropertySvg_24 } from './grid-property-24.svg?raw';
 export { default as LayerPolygonSvg_12 } from './layer-polygon-12.svg?raw';
 export { default as LayerPolygonSvg_16 } from './layer-polygon-16.svg?raw';
 export { default as LayerPolygonSvg_32 } from './layer-polygon-32.svg?raw';
@@ -37,6 +47,14 @@ export { default as LayerStarSvg_12 } from './layer-star-12.svg?raw';
 export { default as LayerStarSvg_16 } from './layer-star-16.svg?raw';
 export { default as LayerStarSvg_32 } from './layer-star-32.svg?raw';
 export { default as LayerWidgetSvg_16 } from './layer-widget-16.svg?raw';
+export { default as NumberPropertySvg_16 } from './number-property-16.svg?raw';
+export { default as NumberPropertySvg_24 } from './number-property-24.svg?raw';
+export { default as TextInputPropertySvg_16 } from './text-input-property-16.svg?raw';
+export { default as TextInputPropertySvg_24 } from './text-input-property-24.svg?raw';
+export { default as TextPropertySvg_16 } from './text-property-16.svg?raw';
+export { default as TextPropertySvg_24 } from './text-property-24.svg?raw';
+export { default as TogglePropertySvg_16 } from './toggle-property-16.svg?raw';
+export { default as TogglePropertySvg_24 } from './toggle-property-24.svg?raw';
 export { default as ToolbarActionsSvg } from './toolbar-actions.svg?raw';
 export { default as ToolbarArrowSvg } from './toolbar-arrow.svg?raw';
 export { default as ToolbarCommentSvg } from './toolbar-comment.svg?raw';
@@ -56,4 +74,10 @@ export { default as ToolbarSectionSvg } from './toolbar-section.svg?raw';
 export { default as ToolbarSliceSvg } from './toolbar-slice.svg?raw';
 export { default as ToolbarStarSvg } from './toolbar-star.svg?raw';
 export { default as ToolbarTextSvg } from './toolbar-text.svg?raw';
+export { default as VariableModePropertySvg_16 } from './variable-mode-property-16.svg?raw';
+export { default as VariableModePropertySvg_24 } from './variable-mode-property-24.svg?raw';
+export { default as VariablePropertySvg_16 } from './variable-property-16.svg?raw';
+export { default as VariablePropertySvg_24 } from './variable-property-24.svg?raw';
+export { default as VariantPropertySvg_16 } from './variant-property-16.svg?raw';
+export { default as VariantPropertySvg_24 } from './variant-property-24.svg?raw';
 //@endindex
