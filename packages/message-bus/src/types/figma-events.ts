a88761aa7dd4a7e7de6698c6132b1c6142f3d991
Alberto Nunes <bisneto.alberto@gmail.com>
2024-12-13 19:40:00 -0300

    events docs

--- packages/message-bus/src/types/figma-events.ts
+++ packages/message-bus/src/types/figma-events.ts
@@ -7,91 +7,79 @@ import type { EventRegistry } from './registries';
  */
 export type CoreEvents = Jsonify<{
   /**
-   * This event will trigger when the selection of the current page changes.
-   *
-   * Note also that changing the selection via the plugin API,
-   * then changing it back to its previous value immediately still triggers the event.
-   *
-   * @remarks
-   * This can happen:
-   *  - By user action.
-   *  - Due to plugin code.
-   *  - When the current page changes
-   *    (a "currentpagechange" event always triggers a "selectionchange" event).
-   *  - When a selected node is deleted.
-   *  - When a selected node becomes the child of another selected node
-   *    (in which case it is considered indirectly selected,
-   *    and is no longer in figma.currentPage.selection)
+   * Triggered when the selection changes in the current page
    */
   SelectionChanged: () => void;
 
   /**
-   * This event will trigger when the user navigates to a different page,
-   * or when the plugin changes the value of figma.currentPage.
+   * Triggered when the user switches to a different page or when 
+   * figma.currentPage is changed programmatically
    */
   CurrentPageChanged: () => void;
 
   /**
-   * This event will trigger when a change is made to the currently open file.
-   *
-   * The event will be called when nodes/styles are either added, removed, or changed in a document.
-   *
-   * @remarks
-   * Note that DocumentChangeEvent has a documentChanges property with an array of DocumentChanges.
-   * Figma will not call the 'documentchange' callback synchronously and will instead batch the
-   * updates and send them to the callback periodically
-   *
-   * There are many different DocumentChange types. Check the Figma API documentation for more details.
-   * @see {@link https://www.figma.com/plugin-docs/api/DocumentChange/|Figma API documentation}
+   * Triggered when any changes are made to the document
    */
   DocumentChanged: DocumentChangeEvent;
 
   /**
-   * This event will trigger when objects outside Figma (such as elements from other browser windows,
-   * or files from the local filesystem) are dropped onto the canvas.
-   *
-   * It can also be triggered by a special pluginDrop message sent from the UI.
-   *
-   * See the {@link https://www.figma.com/plugin-docs/creating-ui/#triggering-drop-events-from-the-ui|Triggering drop events from the UI} section for more details.
+   * Triggered when external content is dropped onto the canvas
    */
   OnDrop: DropEvent;
 
   /**
-   * This event will trigger when the plugin is about to close, either from
-   * a call to figma closePlugin() or the user closing the plugin via the UI.
-   *
-   * ! You should use this API only if strictly necessary,
-   * ! and run as little code as possible in the callback when doing so.
-   *
-   * This is a good place to run cleanup actions. For example, some plugins
-   * add UI elements in the canvas by creating nodes. These UI elements should
-   * be deleted when the plugin is closed.
-   *
-   * @remarks
-   * Note that you don't need to call figma.closePlugin() again in this function.
-   *
-   * When a user closes a plugin, they expect it to be closed immediately.
-   * Having long-running actions in the closing callback prevents the plugin for closing promptly.
-   *
-   * This is also not the place to run any asynchronous actions (e.g. register callbacks, using
-   * await, etc). The plugin execution environment will be destroyed immediately when all the
-   * callbacks have returned, and further callbacks will not be called.
+   * Triggered when the plugin is about to close
    */
   OnClose: () => void;
 
   /**
-   * This event is triggered when a plugin is run.
-   *
-   * Handling the run event is only required for plugins with parameters.
-   *
-   * @remarks
-   * For all plugins it can still be a convenient spot to put your top level code,
-   * since it is called on every plugin run.
-   *
-   * For plugins with parameters, this happens after all parameters have been enter by the user
-   * in the quick action UI. For all other plugins this happens immediately after launch.
+   * Triggered when the plugin starts
    */
   OnRun: RunEvent;
+
+  /**
+   * Triggered when a node is created in the document
+   * @since Figma 2.0
+   */
+  CreateNode: {
+    node: SceneNode;
+    parent: BaseNode | null;
+  };
+
+  /**
+   * Triggered when a node is deleted from the document
+   * @since Figma 2.0
+   */
+  DeleteNode: {
+    node: SceneNode;
+  };
+
+  /**
+   * Triggered when the viewport changes (pan/zoom)
+   * @since Figma 2.0
+   */
+  ViewportChanged: {
+    viewport: {
+      bounds: Rect;
+      zoom: number;
+    };
+  };
+
+  /**
+   * Triggered when the user interacts with a plugin menu item
+   * @since Figma 2.0
+   */
+  PluginMenuItemClick: {
+    menuId: string;
+  };
+
+  /**
+   * Triggered when the network status changes
+   * @since Figma 2.0
+   */
+  NetworkStatusChanged: {
+    isOnline: boolean;
+  };
 }>;
 
 /**
@@ -113,7 +101,9 @@ export type FigmaEventName = keyof CoreEvents | keyof TimerEvents;
 export type FigmaEventRegistry = Simplify<
   EventRegistry<CoreEvents, 'figma'> | EventRegistry<TimerEvents, 'timer'>
 >;
-declare type ArgFreeEventType =
+
+// Event types that don't take arguments
+export type ArgFreeEventType =
   | 'selectionchange'
   | 'currentpagechange'
   | 'close'
@@ -122,7 +112,9 @@ declare type ArgFreeEventType =
   | 'timerpause'
   | 'timerresume'
   | 'timeradjust'
-  | 'timerdone';
+  | 'timerdone'
+  | 'networkstatuschange';
+
 // Enum for backward compatibility
 export enum FigmaEvent {
   SelectionChanged = 'figma:event/selection-changed',
@@ -131,12 +123,17 @@ export enum FigmaEvent {
   OnDrop = 'figma:event/on-drop',
   OnClose = 'figma:event/on-close',
   OnRun = 'figma:event/on-run',
+  CreateNode = 'figma:event/create-node',
+  DeleteNode = 'figma:event/delete-node',
+  ViewportChanged = 'figma:event/viewport-changed',
+  PluginMenuItemClick = 'figma:event/plugin-menu-item-click',
+  NetworkStatusChanged = 'figma:event/network-status-changed',
   TimerStarted = 'timer:event/timer-started',
   TimerPaused = 'timer:event/timer-paused',
   TimerStopped = 'timer:event/timer-stopped',
   TimerDone = 'timer:event/timer-done',
   TimerResume = 'timer:event/timer-resume',
-  TimerAdjust = 'timer:event/timer-adjust',
+  TimerAdjust = 'timer:event/timer-adjust'
 }
 
 // Event definition type (kept for backward compatibility)
