b3a4da7ec1d4c0a792b570ef8ed3049516fd89b6
Saulo Vallory <me@saulo.engineer>
2024-12-18 15:53:36 -0300

    chore: add biome config to rpc package and run lint
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- packages/rpc/src/createClient.ts
+++ packages/rpc/src/createClient.ts
@@ -1,50 +1,50 @@
-import { sendRequest } from "./rpc";
+import { sendRequest } from './rpc';
 import type {
-	JsonValue,
-	MakeAllFnAsync,
-	MakeFnAsync,
-	RpcClientOptions,
-} from "./types";
+  JsonValue,
+  MakeAllFnAsync,
+  MakeFnAsync,
+  RpcClientOptions,
+} from './types';
 
 function listMethods(classConstructor: NewableFunction): string[] {
-	const prototype = classConstructor.prototype;
-	const props = Object.getOwnPropertyNames(prototype);
+  const prototype = classConstructor.prototype;
+  const props = Object.getOwnPropertyNames(prototype);
 
-	const methods = props.filter((prop) => {
-		const descriptor = Object.getOwnPropertyDescriptor(prototype, prop);
+  const methods = props.filter((prop) => {
+    const descriptor = Object.getOwnPropertyDescriptor(prototype, prop);
 
-		if (
-			!descriptor ||
-			prop === "constructor" ||
-			prop.startsWith("_") ||
-			prop.startsWith("#")
-		) {
-			return false;
-		}
+    if (
+      !descriptor ||
+      prop === 'constructor' ||
+      prop.startsWith('_') ||
+      prop.startsWith('#')
+    ) {
+      return false;
+    }
 
-		return (
-			typeof descriptor.value === "function" && descriptor.enumerable === true
-		);
-	});
+    return (
+      typeof descriptor.value === 'function' && descriptor.enumerable === true
+    );
+  });
 
-	return methods;
+  return methods;
 }
 
 export function createClient<T extends new (...args: unknown[]) => unknown>(
-	stubClass: T,
-	options?: RpcClientOptions,
+  stubClass: T,
+  options?: RpcClientOptions,
 ): MakeAllFnAsync<InstanceType<T>> {
-	const timeout = options?.timeout;
-	const methods = listMethods(stubClass);
-	const stub = {} as MakeAllFnAsync<InstanceType<T>>;
+  const timeout = options?.timeout;
+  const methods = listMethods(stubClass);
+  const stub = {} as MakeAllFnAsync<InstanceType<T>>;
 
-	for (const method of methods) {
-		const methodFn = (...params: JsonValue[]) =>
-			sendRequest(method, params, timeout);
-		stub[method as keyof typeof stub] = methodFn as MakeFnAsync<
-			InstanceType<T>[keyof InstanceType<T>]
-		>;
-	}
+  for (const method of methods) {
+    const methodFn = (...params: JsonValue[]) =>
+      sendRequest(method, params, timeout);
+    stub[method as keyof typeof stub] = methodFn as MakeFnAsync<
+      InstanceType<T>[keyof InstanceType<T>]
+    >;
+  }
 
-	return stub;
+  return stub;
 }
