7f90744fc78da9dae73746ee6c61ed09bd6d5579
Saulo Vallory <me@saulo.engineer>
2024-12-23 18:42:58 -0300

    style: lint
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- packages/message-bus/src/types.ts
+++ packages/message-bus/src/types.ts
@@ -1,5 +1,5 @@
-import type { CommandRegistry } from "./commands";
-import type { EventRegistry } from "./events";
+import type { CommandRegistry } from './commands';
+import type { EventRegistry } from './events';
 
 /**
  * A union type of all possible values of type T
@@ -9,29 +9,29 @@ export type SomeObject = Record<string, unknown>;
 export type AutocompletableString = string | number | symbol;
 
 export type MessageData<
-	TCmd = unknown,
-	TEvt = unknown,
-	Key extends keyof CommandRegistry<TCmd> | keyof EventRegistry<TEvt> = never,
+  TCmd = unknown,
+  TEvt = unknown,
+  Key extends keyof CommandRegistry<TCmd> | keyof EventRegistry<TEvt> = never,
 > = Key extends keyof CommandRegistry<TCmd>
-	? CommandRegistry<TCmd>[Key]["message"]
-	: Key extends keyof EventRegistry<TEvt>
-		? EventRegistry<TEvt>[Key]["message"]
-		: never;
+  ? CommandRegistry<TCmd>[Key]['message']
+  : Key extends keyof EventRegistry<TEvt>
+    ? EventRegistry<TEvt>[Key]['message']
+    : never;
 
 type Handler<TCmd, Command extends SomeValueOf<CommandRegistry<TCmd>>> = (
-	message: Command["message"],
-) => Command["result"];
+  message: Command['message'],
+) => Command['result'];
 
 type Listener<TEvt, Event extends SomeValueOf<EventRegistry<TEvt>>> = (
-	message: Event["message"],
+  message: Event['message'],
 ) => void;
 
 export type CommandHandlers<TCmd = unknown> = {
-	[K in keyof CommandRegistry<TCmd>]: Handler<TCmd, CommandRegistry<TCmd>[K]>;
+  [K in keyof CommandRegistry<TCmd>]: Handler<TCmd, CommandRegistry<TCmd>[K]>;
 };
 
 export type EventListeners<TEvt = unknown> = {
-	[K in keyof EventRegistry<TEvt>]: Listener<TEvt, EventRegistry<TEvt>[K]>;
+  [K in keyof EventRegistry<TEvt>]: Listener<TEvt, EventRegistry<TEvt>[K]>;
 };
 
 export type DeregisterFn = () => void;
--- packages/message-bus/src/utils.ts
+++ packages/message-bus/src/utils.ts
@@ -1,40 +1,40 @@
 interface MapObject {
-	dataType: "Map";
-	value: [unknown, unknown][];
+  dataType: 'Map';
+  value: [unknown, unknown][];
 }
 
 export function JsonReplacer(_: string, value: unknown) {
-	if (value instanceof Map) {
-		return {
-			dataType: "Map",
-			value: Array.from(value.entries()), // or with spread: value: [...value]
-		};
-	}
+  if (value instanceof Map) {
+    return {
+      dataType: 'Map',
+      value: Array.from(value.entries()), // or with spread: value: [...value]
+    };
+  }
 
-	return value;
+  return value;
 }
 
 export function JsonReviver(_: string, value: unknown) {
-	if (typeof value === "object" && value !== null) {
-		const mapObj = value as MapObject;
-		if (mapObj.dataType === "Map") {
-			return new Map<unknown, unknown>(mapObj.value);
-		}
-	}
-	return value;
+  if (typeof value === 'object' && value !== null) {
+    const mapObj = value as MapObject;
+    if (mapObj.dataType === 'Map') {
+      return new Map<unknown, unknown>(mapObj.value);
+    }
+  }
+  return value;
 }
 
 export function serialize(obj: unknown): string {
-	return JSON.stringify(obj, JsonReplacer);
+  return JSON.stringify(obj, JsonReplacer);
 }
 
 export function deserialize<T>(obj: unknown): T {
-	if (typeof obj !== "string") {
-		throw new Error("Input must be a string");
-	}
-	return JSON.parse(obj, JsonReviver);
+  if (typeof obj !== 'string') {
+    throw new Error('Input must be a string');
+  }
+  return JSON.parse(obj, JsonReviver);
 }
 
 export function deepClone<T>(obj: T): T {
-	return deserialize(serialize(obj)) as T;
+  return deserialize(serialize(obj)) as T;
 }
