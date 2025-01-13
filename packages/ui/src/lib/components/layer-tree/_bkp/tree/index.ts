3e34f58f9cb544a5ddc5e2afab94a48ef231e2c4
Saulo Vallory <me@saulo.engineer>
2025-01-13 10:32:17 -0300

    chore: cleanup
    Signed-off-by: Saulo Vallory <me@saulo.engineer>

--- packages/ui/src/lib/components/layer-tree/_bkp/tree/index.ts
+++
@@ -1,8 +0,0 @@
-//@index(['./[^\.]+.svelte'], (f, {pascalCase}) => `export { default as ${pascalCase(f.path)} } from '${f.path}${f.ext}';`)
-export { default as TreeNode } from './tree-node.svelte';
-export { default as Tree } from './tree.svelte';
-//@endindex
-
-//@index(['./[^\.]+.ts'], (f, {pascalCase}) => `export * from '${f.path}';`)
-export * from './types';
-//@endindex
